7ef6b5f92a92a71cbcd857437822db332449250c
Aleksandr Khomakha <alexfcdp@gmail.com>
2018-08-18 22:30:52 +0300

    added view books; books_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'books/index'
+  get 'books/show'
   get 'home/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users, ActiveAdmin::Devise.config
