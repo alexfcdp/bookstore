36c617537f62f36bbbb044bc53004db4b294fb85
Aleksandr Khomakha <alexfcdp@gmail.com>
2019-01-03 12:46:53 +0200

    added some tests for services

--- spec/factories/credit_cards.rb
+++ spec/factories/credit_cards.rb
@@ -2,7 +2,7 @@
 
 FactoryBot.define do
   factory :credit_card do
-    number { rand(1000..9999) }
+    number { rand(1000..9999).to_s }
     card_owner { FFaker::Lorem.word }
     expiry_date { '12/20' }
     order
